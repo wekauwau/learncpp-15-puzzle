e6e461411b4836a738a756a99df71d100ebf67ed
wekauwau <wekauwau@gmail.com>
2026-03-14 23:16:49 +0700

    feat: now we plei

--- src/Direction.hpp
+++ src/Direction.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <optional>
 #include <ostream>
 #include <wekauwau/random.hpp>
 
 struct Direction {
   enum class Type { Up, Left, Down, Right };
+
+  static std::optional<Direction> from(char c) {
+    switch (c) {
+    case 'w':
+      return Direction{Type::Up};
+    case 'a':
+      return Direction{Type::Left};
+    case 's':
+      return Direction{Type::Down};
+    case 'd':
+      return Direction{Type::Right};
+    default:
+      return std::nullopt;
+    }
+  }
 
   Type direction;
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,70 @@
 #include "Board.hpp"
-#include <array>
+#include "Direction.hpp"
 #include <iostream>
-#include <variant>
+#include <termios.h>
+#include <unistd.h>
 
 // Keep it on the stack
 
+#ifndef BOARD_WIDTH
+#define BOARD_WIDTH 4
+#endif
+
+#ifndef BOARD_HEIGHT
+#define BOARD_HEIGHT 4
+#endif
+
+char getChar() {
+  termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~static_cast<tcflag_t>(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  char c{static_cast<char>(getchar())};
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return c;
+}
+
+void printStep(int step) { std::cout << step << " step" << (step < 2 ? " " : "s "); }
+
 int main() {
-  using AnyBoard = std::variant<Board<4, 4>, Board<3, 5>, Board<4, 6>>;
-  std::array<AnyBoard, 3> boards{Board<4, 4>{}, Board<3, 5>{}, Board<4, 6>{}};
-  // std::array<AnyBoard, 1> boards{Board<4, 4>{}};
+  Board<BOARD_WIDTH, BOARD_HEIGHT> board;
+  board.shuffle();
 
-  for (auto& b : boards)
-    std::visit(
-        [](auto& board) {
-          std::cout << board << '\n'
-                    << "This board is " << (board.isSolved() ? "" : "not ") << "solved" << '\n'
-                    << "Empty tile is in " << board.getEmptyTile() << '\n'
-                    << "Shuffling...\n\n";
+  std::cout << "Welcome to " << (BOARD_WIDTH * BOARD_HEIGHT) - 1 << "-Puzzle!\n"
+            << "Now, solve this shit!\n"
+            << "w/a/s/d to slide the tile, q to quit.\n"
+            << board << '\n';
 
-          board.shuffle();
-          std::cout << board << '\n'
-                    << "This board is shuffled, ready to play!\n"
-                    << "Empty tile is in " << board.getEmptyTile() << "\n\n\n\n\n";
-        },
-        b);
+  int step{0};
+  while (!board.isSolved()) {
+    std::cout << "Enter a command: ";
+    auto input{getChar()};
+
+    if (input == 'q') {
+      std::cout << "\n\n";
+      printStep(step);
+      std::cout << "of despair...\n"
+                << "Bye!\n\n";
+
+      return 0;
+    }
+
+    auto direction{Direction::from(input)};
+    if (!direction)
+      std::cout << "🤡💩\n";
+    else if (!board.slide(*direction))
+      std::cout << "🦍💩\n";
+    else {
+      std::cout << '\n' << BoardView{board} << '\n';
+      ++step;
+    }
+  }
+
+  std::cout << "\n\n"
+            << "GGWP!\n";
+  printStep(step);
+  std::cout << "though.\n\n";
 
   return 0;
 }
